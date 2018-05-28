3ade9a55fecae0eb66da5ad021d251c326173e3e
Austin Benson <arbenson@gmail.com>
2018-05-28 16:27:18 -0400

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,30 @@ else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+function basic_undir_test1()
+    I = [1, 1, 2, 3, 3]
+    J = [2, 3, 3, 4, 5]
+    A = sparse(I, J, 1, 5, 5)
+
+    global_clcf = (2 + 2 + 2 + 0 + 0) / (4 + 4 + 2 + 3 + 3)
+    avg_clcf    = mean([1.0 / 2, 1.0 / 2, 1.0, 0.0, 0.0])
+    local_clcfs =      [1.0 / 2, 1.0 / 2, 1.0, 0.0, 0.0]
+    wedges      = [4, 4, 2, 3, 3]
+    triangles   = [1, 1, 1, 0, 0]    
+
+    C = copy(A)
+    C[1, 1] = 1
+    for B in [max.(A, A'), A, C, max.(C, C')]
+        ret = undir_clcfs(B)
+        @test ret.global_clcf  ≈ global_clcf
+        @test ret.avg_clcf     ≈ avg_clcf
+        @test ret.local_clcfs  ≈ local_clcfs      
+        @test ret.wedges == wedges
+        @test ret.triangles  == triangles
+    end
+end
+
+function run()
+    basic_undir_test1()
+end
+run()
